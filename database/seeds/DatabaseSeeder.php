41d1da0015e22d7fae350ed8ed56c5d6a2a7a88a
Liviu Coroncicu <liviucoronciuc@gmail.com>
2015-11-24 21:04:56 +0100

    Added data in task and tags table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Tag;
+use App\Task;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,6 +18,39 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserTableSeeder::class);
 
+        $faker = Faker\Factory::create();
+        $this->seedTasks($faker);
+        $this->seedTags($faker);
+
         Model::reguard();
+    }
+
+
+    /**
+     * @param $faker
+     */
+    private function seedTasks($faker)
+    {
+        foreach (range(0, 100) as $number) {
+            $task = new Task();
+
+            $task->name = $faker->sentence;
+            $task->done = $faker->boolean;
+            $task->priority = $faker->randomDigit;
+
+            $task->save();
+        }
+    }
+
+
+    private function seedTags($faker)
+    {
+        foreach (range(0, 100) as $number) {
+            $tags = new Tag();
+
+            $tags->name = $faker->name;
+
+            $tags->save();
+        }
     }
 }
